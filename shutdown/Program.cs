cdeae7b0506a82a1a9d3327cd4a2d0d638578b50
Bluscream <Bluscream@users.noreply.github.com>
2022-12-23 01:09:59 +0100

    add /skip option

--- shutdown/Program.cs
+++ shutdown/Program.cs
@@ -1,25 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Shutdown_Confirm
 {
-	internal static class Program
-	{
+	internal static class Program {
+		private static List<string> allowedArgs = new List<string>(new[] { "/a", "/skip" });
 		[STAThread]
-		private static void Main(string[] args)
-		{
-			string argstr = string.Join(" ", args);
-			if (argstr == "/a" || MessageBox.Show($"Some app requested a shutdown, accept?\n\n{argstr}", "Shutdown request", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+		private static void Main(string[] args) {
+            string argstr = string.Join(" ", args);
+            foreach (var arg in args) {
+				if (allowedArgs.Contains(arg)) Shutdown(argstr);
+			}
+			if (MessageBox.Show($"Some app requested a shutdown, accept?\n\n{argstr}", "Shutdown request", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
 			{
-				Process.Start(new ProcessStartInfo {
-					FileName = "shutdown_.exe",
-					Arguments = argstr,
-					CreateNoWindow = true,
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    UseShellExecute = false
-            }) ;
+				Shutdown(argstr);
 			}
 		}
+		private static void Shutdown(string argstr) {
+            Process.Start(new ProcessStartInfo {
+                FileName = "shutdown_.exe",
+                Arguments = argstr.Replace("/skip",""),
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false
+            });
+			Environment.Exit(0);
+        }
 	}
 }
