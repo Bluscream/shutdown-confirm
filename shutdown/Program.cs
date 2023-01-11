ad3d6fff7d61fcdff0209bde932dbb6c0abe31ba
Bluscream <Bluscream@users.noreply.github.com>
2023-01-11 02:31:21 +0100

    idk lol

--- shutdown/Program.cs
+++ shutdown/Program.cs
@@ -1,31 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Shutdown_Confirm
 {
 	internal static class Program {
-		private static List<string> allowedArgs = new List<string>(new[] { "/a", "/skip" });
+		private static List<string> allowedArgs = new List<string>(new[] { "/skip" });
 		[STAThread]
 		private static void Main(string[] args) {
             string argstr = string.Join(" ", args);
-            foreach (var arg in args) {
-				if (allowedArgs.Contains(arg)) Shutdown(argstr);
-			}
-			if (MessageBox.Show($"Some app requested a shutdown, accept?\n\n{argstr}", "Shutdown request", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+			var app = "shutdown";
+			var skip = argstr.Contains(allowedArgs[0]);
+			argstr = argstr.Replace(allowedArgs[0], "");
+			foreach (var arg in args) {
+				if (arg.StartsWith("app:")) {
+					app = arg.Replace("app:", "");
+                    argstr= argstr.Replace("app:", "").Replace(app, "");
+                }
+                var matches = new Regex("app:(.*)").Matches(argstr);
+                if (matches.Count > 0) {
+                    argstr = argstr.Replace(matches[0].Groups[0].Value, "");
+                    app = matches[0].Groups[1].Value;
+                }
+            }
+
+            if (skip || MessageBox.Show($"Some app wants to run {app}, accept?\n\n{argstr}", $"{app} request", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
 			{
-				Shutdown(argstr);
+				Shutdown(app, argstr);
 			}
 		}
-		private static void Shutdown(string argstr) {
-            Process.Start(new ProcessStartInfo {
-                FileName = "shutdown_.exe",
-                Arguments = argstr.Replace("/skip",""),
-                CreateNoWindow = true,
-                WindowStyle = ProcessWindowStyle.Hidden,
-                UseShellExecute = false
-            });
+		private static void Shutdown(string app, string argstr) {
+			var proc = new ProcessStartInfo {
+				FileName = app,
+				Arguments = argstr,
+				CreateNoWindow = true,
+				WindowStyle = ProcessWindowStyle.Hidden,
+				UseShellExecute = false
+			};
+            Process.Start(proc);
 			Environment.Exit(0);
         }
 	}
